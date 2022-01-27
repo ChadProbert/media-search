d8ced67284f58ed7945f210901910e43df0986a7
Chad Probert <chadcprobert@gmail.com>
2022-01-27 22:29:24 +0200

    Add files via upload

---
+++ backend/frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+// components
+import Menu from "./components/Menu.js";
+import Search from "./components/Search.js";
+import SearchResults from "./components/SearchResults.js";
+
+// stylesheets
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<Menu />
+			<br />
+			<Search />
+			<br />
+			<SearchResults />
+		</div>
+	);
+}
+
+export default App;
